3235bdacc7e6c4e362b763af54d7b62f56877ed2
shantanunarale <shantanunarale062@gmail.com>
2024-04-14 14:44:47 +0530

    Fixed VehicleDocumentType Table Schema

--- dbo/Tables/VehicleDocumentType.sql
+++ dbo/Tables/VehicleDocumentType.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[VehicleDocumentType] (
     [VehicleDocumentTypeId]         INT           IDENTITY (1, 1) NOT NULL,
-    [VehicleDocumentTypeId]         INT           IDENTITY (1, 1) NOT NULL,
+    [VehicleDocumentType]           VARCHAR (100) NOT NULL,
     [RequireExpirationReminder]     BIT           NULL,
     [VehicleDocumentDescription]    VARCHAR(50)   NULL,
     PRIMARY KEY CLUSTERED ([VehicleDocumentTypeId] ASC)
