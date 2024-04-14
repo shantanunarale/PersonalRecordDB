3d999667c393a07d5fd0b33cfde5598d0cae364e
shantanunarale <shantanunarale062@gmail.com>
2024-04-14 15:09:48 +0530

    Development complete.

---
+++ PostDeploy/Static_Insert_DocumentType.PostDeploy.sql
@@ -0,0 +1,14 @@
+INSERT INTO dbo.DocumentType(DocumentType, DocumentTypeDescription)
+VALUES('Aadhar', 'Aadhar Card issued by UIDAI'),
+('PAN', 'Permanent Account Number card issued by Income Tax.'),
+('DL', 'Driving License issued by RTO.'),
+('PRAN', 'Permanent Retirement Account Number'),
+('MC', 'Marriage Certificate'),
+('Passport', 'Individual Passport.'),
+('SSC', 'SSC Mark sheet and Diploma.'),
+('HSC', 'HSC Mark sheet and Diploma.'),
+('Grad', 'Graduation Mark sheet and Diploma.'),
+('PG', 'Post Graduation Mark sheet and Diploma.'),
+('DOB', 'Date of Birth Certificate.'),
+('Misc', 'Miscellaneous Documents.')
+
