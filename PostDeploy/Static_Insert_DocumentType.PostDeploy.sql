63bb75f127d46b2f7dbc7d013ed6cf0ece9d636d
shantanunarale <shantanunarale062@gmail.com>
2024-04-28 15:58:44 +0530

    Dev Complete.

--- PostDeploy/Static_Insert_DocumentType.PostDeploy.sql
+++ PostDeploy/Static_Insert_DocumentType.PostDeploy.sql
@@ -1,15 +1,16 @@
-INSERT INTO dbo.DocumentType(DocumentType, DocumentTypeDescription)
-VALUES('Aadhar', 'Aadhar Card issued by UIDAI'),
-('PAN', 'Permanent Account Number card issued by Income Tax.'),
-('DL', 'Driving License issued by RTO.'),
-('PRAN', 'Permanent Retirement Account Number'),
-('MC', 'Marriage Certificate'),
-('Passport', 'Individual Passport.'),
-('SSC', 'SSC Mark sheet and Diploma.'),
-('HSC', 'HSC Mark sheet and Diploma.'),
-('Grad', 'Graduation Mark sheet and Diploma.'),
-('PG', 'Post Graduation Mark sheet and Diploma.'),
-('DOB', 'Date of Birth Certificate.'),
-('Misc', 'Miscellaneous Documents.'),
-('Sign', 'Signature Image')
+INSERT INTO dbo.DocumentType(DocumentType, DocumentTypeDescription, ExpirationReminder)
+VALUES('Aadhar', 'Aadhar Card issued by UIDAI', 0),
+('PAN', 'Permanent Account Number card issued by Income Tax.', 0),
+('DL', 'Driving License issued by RTO.', 1),
+('PRAN', 'Permanent Retirement Account Number', 0),
+('MC', 'Marriage Certificate', 0),
+('Passport', 'Individual Passport.', 1),
+('SSC', 'SSC Mark sheet and Diploma.', 0),
+('HSC', 'HSC Mark sheet and Diploma.', 0),
+('Grad', 'Graduation Mark sheet and Diploma.', 0),
+('PG', 'Post Graduation Mark sheet and Diploma.', 0),
+('DOB', 'Date of Birth Certificate.', 0),
+('Misc NE', 'Miscellaneous Documents Non Expiry.', 0),
+('Sign', 'Signature Image', 0),
+('Misc E', 'Miscellaneous Documents Expiry.', 1)
 
